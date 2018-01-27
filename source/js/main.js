c7778eaed3ae4455c4cf463338587996081b4bae
Taras <tartitan1999@gmail.com>
2018-01-27 05:22:20 +0200

    add main.js : tvsy6520@gmail.com

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-console.log('Hello world!');
+
+// Back to Top - btn
+var btnTop = $('.btn-top');
+
+$(window).on('scroll', function(){
+	if($(window).scrollTop() >= 670){
+		btnTop.fadeIn();
+	} else {
+		btnTop.fadeOut();
+	};
+});
+
+btnTop.on('click', function(){
+	$('html, body').animate({scrollTop:0}, 1000);
+});
+
+
+// Scrolling anchor
+$('a[data-target^="anchor"]').bind('click.smoothscroll', function(){
+	var elementClick = $(this).attr('href'),
+		destination = $(elementClick).offset().top;
+
+	$('html, body').animate({scrollTop: destination}, 1000);
+	return false;
+});
